8eab4c5b7d936e925df02274c37964d0b3f416b4
irubioph <delightsmediaph@gmail.com>
2023-07-01 16:34:07 +0800

    feat: Added Portfolio & some updates to About

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,11 @@
 const About = () => {
     return ( 
-        <div>
+        <p>
             <h1>About</h1>
-        </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit doloribus, officia eligendi debitis eos? Ab, eligendi dolorem. Quos, voluptas! Aperiam rerum ducimus corrupti provident possimus corporis aut veniam recusandae.</p>
+            <p><Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos expedita deserunt, praesentium eius distinctio, inventore fuga, dolore laudantium in commodi optio. Possimus accusantium temporibus saepe repudiandae vero culpa iusto quod!></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus illum nesciunt voluptatem velit eos, ab commodi consectetur quasi reprehenderit iste, ipsum inventore optio. Cupiditate itaque magni ad repellat quae nostrum?</p>
+        </p>
      );
 }
  
