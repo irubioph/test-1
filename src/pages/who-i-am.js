e5ed7fce83d5bad8434bc5aa1958beb51654f956
Ivan Rubio <ivan@thepetlabco.com>
2023-07-24 03:45:26 +0800

    feat: adding footer nine

---
+++ src/pages/who-i-am.js
@@ -0,0 +1,15 @@
+import FooterNine from "@/components/Footers/FooterNine";
+
+FooterNine
+
+const whoIam = () => {
+    return ( 
+        <>
+        <h1>Who I am</h1>
+
+        <FooterNine />
+        </>
+     );
+}
+ 
+export default whoIam;
