e8022870216aa65ec6f7b64acdefaafcdad88290
Ivan Rubio <ivan@thepetlabco.com>
2023-07-24 04:53:39 +0800

    add some stuff

--- src/pages/who-i-am.js
+++ src/pages/who-i-am.js
@@ -4,7 +4,7 @@ const whoIam = () => {
     return ( 
         <>
         <h1>Who I am</h1>
-
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident doloremque soluta debitis pariatur quasi molestias fuga tempora ad reiciendis! Tenetur temporibus saepe rem enim at nesciunt aperiam laudantium aliquid earum?</p>
         <FooterNine />
         </>
      );
